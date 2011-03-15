923b9e681b17e3bacbb9dfdc2ffeb6ef8b53b61f
Adimir Colen <adimircolen@gmail.com>
2011-03-15 18:58:49 -0300

    change generators whith gem rails3-generators

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,12 @@ module Agenda
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    config.generators do |g|
+      # g.template_engine :haml
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
